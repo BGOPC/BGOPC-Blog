b8bf7f19bf5e78928dcba4c7f5889342660ff719
BGOPC <bb8933400@gmail.com>
2022-07-13 15:59:53 +0430

    Updating Some templates

--- weblog/settings.py
+++ weblog/settings.py
@@ -137,4 +137,5 @@ TAILWIND_APP_NAME = 'theme'
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
-NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
+# NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
+NPM_BIN_PATH = r"/usr/bin/npm"
