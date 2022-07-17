02de35901bc22198f8ce8ccdd1e952f0b9edc36d
BGOPC <bb8933400@gmail.com>
2022-07-17 18:10:57 +0430

    404 page updated

--- weblog/urls.py
+++ weblog/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-
+from django.conf.urls import  handler404
+handler404 = 'weblog.views.handler404'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
---
+++ weblog/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def handler404(request):
+    return render(request, "404.html")
