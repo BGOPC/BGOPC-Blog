710b13cc412d7110f68627fdcfb9cd81598f0cb2
BGOPC <bb8933400@gmail.com>
2022-07-17 17:32:58 +0430

    DB Changed

--- weblog/settings.py
+++ weblog/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9evl$8qf2(t-@=ajl&2pm3pu(z1&6!5l1ed5n=ex^1aya6q+u5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'bgopc-blog.herokuapp.com'
