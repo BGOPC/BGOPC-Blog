4f87651239c564f7d78da91daede77a049a07841
BGOPC <bb8933400@gmail.com>
2022-07-17 18:24:36 +0430

    404 page updated

--- weblog/views.py
+++ weblog/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
-def handler404(request):
-    return render(request, "404.html")
+def handler404(request, exception):
+    return render(request, "404.html", status=404)
