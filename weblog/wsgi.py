1c65733c85b009052c211c8017dfd715628a1752
BGOPC <bb8933400@gmail.com>
2022-07-23 17:29:28 +0430

    I gived Up and gonna put app on debug mode

--- weblog/wsgi.py
+++ weblog/wsgi.py
@@ -10,6 +10,5 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weblog.settings')
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
