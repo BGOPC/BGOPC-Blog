e113dc24dded7e36626cb806e7ad9e00bbffa915
BGOPC <bb8933400@gmail.com>
2022-07-23 19:01:01 +0430

    Damn It

--- weblog/settings.py
+++ weblog/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     'tailwind',
     'theme',
     'copyright',
-    'django_browser_reload',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +57,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 PASSWORD_HASHERS = [
