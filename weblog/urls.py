5658d6aa1d32d6a1e9a8037070d2b6b734a3b029
BGOPC <bb8933400@gmail.com>
2022-07-23 18:56:13 +0430

    Damn It

--- weblog/urls.py
+++ weblog/urls.py
@@ -17,8 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from django.conf.urls import  handler404
-handler404 = 'weblog.views.handler404'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
