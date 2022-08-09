ff581db65c9d38efbe2afea7db62e22c606998ed
BGOPC <bb8933400@gmail.com>
2022-08-09 22:13:27 +0430

    Updating the Security

--- weblog/settings.py
+++ weblog/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 from sys import platform
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#@5@5zbykmff1-4cqj2r0$==*o+t=!u@sww-lrc*x_46i=mb5z'
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = not True
@@ -30,7 +34,8 @@ DEBUG = not True
 ALLOWED_HOSTS = [
     'bgopc-blog.herokuapp.com'
 ]
-
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 # Application definition
 
