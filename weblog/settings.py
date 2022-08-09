e950d815e79c7ab2eabd0c283448038327976c2c
BGOPC <bb8933400@gmail.com>
2022-08-09 22:32:16 +0430

    Reset

--- weblog/settings.py
+++ weblog/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-#@5@5zbykmff1-4cqj2r0$==*o+t=!u@sww-lrc*x_46i=mb5z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = not True
